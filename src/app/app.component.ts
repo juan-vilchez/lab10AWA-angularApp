6ccdcde977e078365dc66610a1fef23be11dd3b2
juan jose vilchez barzola <juan.vilchez@tecsup.edu.pe>
2020-11-27 19:19:43 -0500

    commit completo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ProductosService } from './services/productos.service';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularApp';
+  lista=null;
+  prod = {
+    _id:null,
+    descripcion:null,
+    precio:null
+  }
+  constructor(private productosServicio: ProductosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+  
+  recuperarTodos() {
+    this.productosServicio.listar().subscribe(result => {
+      this.lista = result;
+    });
+  }
+
+  nuevo() {
+    this.productosServicio.nuevo(this.prod).subscribe(result => {
+      if (result=='ok') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  eliminar(codigo:any) {
+  	if(!confirm("Esta seguro que desea eliminar este registro?"))
+  		return;
+    this.productosServicio.eliminar(codigo).subscribe(result => {
+      if (result=='ok') {
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  actualizar() {
+    this.productosServicio.actualizar(this.prod).subscribe(result => {
+      if (result.nModified=='1') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });    
+  }
+  
+  mostrar(codigo:any) {
+    this.productosServicio.mostrar(codigo).subscribe(result => {
+      this.prod = result
+    });
+  }
+
+  hayRegistros() {
+    return true;
+  }
+  limpiar(){
+    this.prod = { 
+      _id:null, 
+      descripcion:null, 
+      precio:null
+    };
+  }
+
+
 }
